a450b28d3dadda3cdbd37e550de86af2d2dfe90b
AnastasiaGWolf <solokhina.g.wolf@gmail.com>
2024-03-12 16:35:04 +0400

    update Login / Reg jsx

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,11 +1,22 @@
 const React = require('react');
 const Layout = require('./Layout');
 
-module.exports = function Main({ login, items }) {
+module.exports = function Main({ login, cards }) {
   return (
     <Layout login={login}>
       <h3 style={{ textAlign: 'center', marginTop: '20px' }}>Magic: The Gathering</h3>
       {/* <script defer src="/js/addFetch.js" /> */}
+      {login ? (
+        <>
+          <div className="filter">
+            <input />
+            <input type="search" />
+          </div>
+          <div className="cardContainer" />
+        </>
+      ) : (
+        <div className="cardContainer" />
+      )}
     </Layout>
   );
 };
