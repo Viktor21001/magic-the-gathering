78047dc2c894a46e31fcc47b8dadc6c720954852
Rusfat116 <rusfat16rus@gmail.com>
2024-03-13 22:35:49 +0300

    add and delete basket

--- public/js/basket.js
+++ public/js/basket.js
@@ -8,15 +8,16 @@ document.addEventListener('DOMContentLoaded', () => {
     // console.log('Ghbdtnd!!!>>>>>!!!!',  event.target);
 
     if (event.target.classList.contains('card-basket-delete')) {
-      const id = event.target.getAttribute('data-cardid');
+      const cardid = event.target.getAttribute('data-cardid');
+      const basketid = event.target.getAttribute('data-basketid');
       // console.log('====>>>>>', event.target);
-      console.log('====>>>>>', id);
+      // console.log('====>>>>>', id);
 
-      console.log('====>>>>>', id);
+      // console.log('====>>>>>', id);
       // const idNum = parseInt(id.match(/\d+/)[0], 10);
 
       try {
-        const response = await fetch(`/basket/${id}`, {
+        let response = await fetch(`/basket/${basketid}`, {
           method: 'DELETE',
         });
         if (response.status === 200) {
